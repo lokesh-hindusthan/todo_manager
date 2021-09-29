c0dda7f0c09bd53d55cd790e394bbfc0da4937ef
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-29 22:32:47 +0530

    Use the resources routing convention for Todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,6 @@
 class TodosController < ApplicationController
-  #def index
-   # render plain: "Hello to rails and welcome! #{DateTime.now.to_s(:short)}"
-   # render plain: Todo.all.to_a
-  #end
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
   end
