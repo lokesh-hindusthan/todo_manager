e4a7034bac7f01d6f7f3d1e53a1a966618e916c9
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-29 18:06:21 +0530

    Added a route that shows a static message

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello to rails! #{DateTime.now.to_s(:short)}"
+    render plain: "Hello to rails and welcome! #{DateTime.now.to_s(:short)}"
   end
 end
