7eb2e8223881e2a4e4396f21eeadf72e4209f1c8
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-10-01 05:47:22 +0530

    The login action in users controller User.find_by with present?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,38 +7,39 @@ class UsersController < ApplicationController
 
   def show
     id = params[:id]
-    #render plain: "The id you wanted was #{id}"
-    users = User.find(id)
-    render plain: users.to_pleasant_string
+    user = User.find(id)
+    render plain: user.to_pleasant_string
   end
 
   def create
-    #render plain: "Hey, this is the the create action!"
-    new_user = User.create!(
-      user_name: params[:user_name],
-      user_email: params[:user_email],
-      user_password: params[:user_password],
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create(
+      user_name: name,
+      user_email: email,
+      user_password: password,
     )
     response_text = "Hey! Your New User is created in the Database by the id  #{new_user.id}"
     render plain: response_text
-        #render plain: "hey, the params are: #{params.to_s}"
   end
+  
   def update
     id = params[:id]
-    user_password = params[:user_password]
+    password = params[:password]
     users = User.find(id)
-    todo.completed = completed
     users.save!
-    render plain: " Hey ! Users Password is Updated and the status to #{user_password}"
+    render plain: " Hey ! Users Password is Updated"
   end
+  
   def login
-    user_email = params[:user_email]
-    user_password = params[:user_password]
+    email = params[:email]
+    password = params[:password]
     #find_by(arg, *args)
     #Finds the first record matching the specified conditions. 
     #There is no implied ordering so if order matters, you should specify it yourself.
     #If no record is found, returns nil
-    user_response = User.find_by(user_email: user_email, user_password: user_password)
-    render plain: user_response
+    user = User.find_by(user_email: email, user_password: password)
+    render plain: user.present?
   end
 end
